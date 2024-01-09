f20bf641ace51c2bf7e396b6320132890b0928de
Er. Hariom Saini <151992853+erhariomsaini@users.noreply.github.com>
2024-01-09 13:39:36 +0530

    Update _footer.php

--- components/_footer.php
+++ components/_footer.php
@@ -1,3 +1,3 @@
 <div class="container-fluid px-0">
-    <p class="text-center bg-dark text-light my-0 py-3">All rights reserved 2024 | My Coding Forum - Coding Discussion</p>
+    <p class="text-center bg-dark text-light my-0 py-3">Copyright 	&#169; 2024, All rights reserved | My Coding Forum - Coding Discussion</p>
 </div>
